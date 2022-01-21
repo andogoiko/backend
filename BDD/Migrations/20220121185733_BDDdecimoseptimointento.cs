f69d272c0865ae436b13dc828fb7f29461a59b06
unknown <andoitz.goico@gmail.com>
2022-01-21 22:32:59 +0100

    BDD perfectamente operativa

---
+++ BDD/Migrations/20220121185733_BDDdecimoseptimointento.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BDD.Migrations
+{
+    public partial class BDDdecimoseptimointento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Provincias",
+                columns: table => new
+                {
+                    Provincia = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provincias", x => x.Provincia);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ZonasRegion",
+                columns: table => new
+                {
+                    Zona = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ZonasRegion", x => x.Zona);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Localidades",
+                columns: table => new
+                {
+                    Localidad = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Latitud = table.Column<double>(type: "float", nullable: true),
+                    Longitud = table.Column<double>(type: "float", nullable: true),
+                    Provincia = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Zona = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Localidades", x => x.Localidad);
+                    table.ForeignKey(
+                        name: "FK_Localidades_Provincias_Provincia",
+                        column: x => x.Provincia,
+                        principalTable: "Provincias",
+                        principalColumn: "Provincia",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Localidades_ZonasRegion_Zona",
+                        column: x => x.Zona,
+                        principalTable: "ZonasRegion",
+                        principalColumn: "Zona",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemporalLocalidades",
+                columns: table => new
+                {
+                    Localidad = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Temperatura = table.Column<double>(type: "float", nullable: true),
+                    VelViento = table.Column<double>(type: "float", nullable: true),
+                    Precipitaciones = table.Column<double>(type: "float", nullable: true),
+                    Humedad = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemporalLocalidades", x => x.Localidad);
+                    table.ForeignKey(
+                        name: "FK_TemporalLocalidades_Localidades_Localidad",
+                        column: x => x.Localidad,
+                        principalTable: "Localidades",
+                        principalColumn: "Localidad",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Localidades_Provincia",
+                table: "Localidades",
+                column: "Provincia");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Localidades_Zona",
+                table: "Localidades",
+                column: "Zona");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TemporalLocalidades");
+
+            migrationBuilder.DropTable(
+                name: "Localidades");
+
+            migrationBuilder.DropTable(
+                name: "Provincias");
+
+            migrationBuilder.DropTable(
+                name: "ZonasRegion");
+        }
+    }
+}
--- BDD/Program.cs
+++ BDD/Program.cs
@@ -14,20 +14,21 @@ namespace BDD
     public class BaseTempoContext : DbContext
     {
 
-        public DbSet<ZonasRegion> ZonaRegion { get; set; }
+        public DbSet<ZonasRegion> ZonasRegion { get; set; }
 
-        public DbSet<Provincias> Provincia { get; set; }
+        public DbSet<Provincias> Provincias { get; set; }
 
-        public DbSet<Localidades> Localidad { get; set; }
+        public DbSet<Localidades> Localidades { get; set; }
 
-        public DbSet<TemporalLocalidades> TemporalLocalidad { get; set; }
+        public DbSet<TemporalLocalidades> TemporalLocalidades { get; set; }
 
         public string connString { get; private set; }
 
         public BaseTempoContext()
         {
-            var database = "BDD06Andoitz"; // "EF{XX}Nombre" => EF00Santi
+            var database = "BDD06Andoitz"; 
             connString = $"Server=185.60.40.210\\SQLEXPRESS,58015;Database={database};User Id=sa;Password=Pa88word;MultipleActiveResultSets=true";
+            //connString = $"Server=(localdb)\\mssqllocaldb;Database=Mierda;Trusted_Connection=True;MultipleActiveResultSets=true";
         }
 
         // protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -47,7 +48,7 @@ namespace BDD
         [Key]
         public string Zona { get; set; }
 
-        public List<Localidades> LocalidadRegi { get; set; }
+        public List<Localidades> Localidades { get; set; }
     }
 
     public class Provincias
@@ -55,31 +56,35 @@ namespace BDD
         [Key]
         public string Provincia { get; set; }
 
-        public List<Localidades> LocalidadProv { get; set; }
+        public List<Localidades> Localidades { get; set; }
     }
 
     public class Localidades
     {
         [Key]
         public string Localidad { get; set; }
-        public double Latitud { get; set; }
-        public double Longitud { get; set; }
-        public string Provincia { get; set; }
-        public string Zona { get; set; }
-        public ZonasRegion ZonaRegionFK { get; set; }
+        public double? Latitud { get; set; }
+        public double? Longitud { get; set; }
 
-        public Provincias ProvinciaFK { get; set; }
+        [Required, ForeignKey("Provincias")]
+        public string Provincia { get; set; }
+
+        [Required, ForeignKey("ZonasRegion")]
+        public string Zona { get; set; }
+        public ZonasRegion ZonasRegion { get; set; }
+
+        public Provincias Provincias { get; set; }
 
     }
 
     public class TemporalLocalidades
     {
-        [Key, ForeignKey("Localidades")]
+        [Key, ForeignKey("LocalidadFK")]
         public string Localidad { get; set; }
-        public double Temperatura { get; set; }
-        public double VelViento { get; set; }
-        public double Precipitaciones { get; set; }
-        public double Humedad { get; set; }
+        public double? Temperatura { get; set; }
+        public double? VelViento { get; set; }
+        public double? Precipitaciones { get; set; }
+        public double? Humedad { get; set; }
 
         public virtual Localidades LocalidadFK { get; set; }
         //esta configuración para crear una relación 0:1 -> https://stackoverflow.com/questions/49305921/entity-framework-1-to-0-or-1-relationship-configuration
